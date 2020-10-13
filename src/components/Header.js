697b3ff81d33584981fcdef52cafeb8cfe492de8
MircleUFO <miracleufodiama@gmail.com>
2020-10-13 14:57:28 +0100

    Header done

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,6 +5,18 @@ import {NavLink} from 'react-router-dom'
 function Header() {
 	return (
 		<header>
+		<img src={icon} alt='Genesys Icon' id='genesysIcon'/>
+			<nav>
+			<div>
+				<NavLink to='/Learnable'>LEARNABLE</NavLink>
+				<NavLink to='#'>AGORA</NavLink>
+				<NavLink to='#'>STARTZONE</NavLink>
+				<NavLink to='#'>ABOUT</NavLink>
+				<NavLink to='#'>CONTACT</NavLink>
+			</div>
+
+			<div id='devStudio'><span>build a product with</span> <button><NavLink to='#'>DEVSTUDIO</NavLink></button></div>
+			</nav>
 		</header>
 	)
 }
